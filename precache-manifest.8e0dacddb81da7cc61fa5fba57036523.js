26cb36cd2e3f1bd48407688c25b7ce391c4951d4
tods_test <todstestuser@gmail.com>
2019-08-30 17:46:13 +0200

    Updates

---
+++ precache-manifest.8e0dacddb81da7cc61fa5fba57036523.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b32982a0150a4f1b19438e7cdd6ba2",
+    "url": "/tods-test/index.html"
+  },
+  {
+    "revision": "01ef22aabffd69c434a6",
+    "url": "/tods-test/static/css/2.3fe6b89b.chunk.css"
+  },
+  {
+    "revision": "e70b1c10da4b0c2456bb",
+    "url": "/tods-test/static/css/main.54a0162f.chunk.css"
+  },
+  {
+    "revision": "01ef22aabffd69c434a6",
+    "url": "/tods-test/static/js/2.33afd3d4.chunk.js"
+  },
+  {
+    "revision": "b7887cfd62528746ee2e",
+    "url": "/tods-test/static/js/3.7bee5ae8.chunk.js"
+  },
+  {
+    "revision": "e70b1c10da4b0c2456bb",
+    "url": "/tods-test/static/js/main.68c35258.chunk.js"
+  },
+  {
+    "revision": "3de0bf74b0fc2f497791",
+    "url": "/tods-test/static/js/runtime~main.626b9c07.js"
+  },
+  {
+    "revision": "5d862cae513110d480cbf13ff064ede6",
+    "url": "/tods-test/static/media/add_to_cart_2.5d862cae.svg"
+  },
+  {
+    "revision": "23a27af8324db942283975bdc17846a3",
+    "url": "/tods-test/static/media/double_coat_beige.23a27af8.webp"
+  },
+  {
+    "revision": "d8bfde94864b9d95b23c74e24a44c481",
+    "url": "/tods-test/static/media/double_coat_beige_alt.d8bfde94.webp"
+  },
+  {
+    "revision": "7b81c75984b6817f6ed52051b1835f4a",
+    "url": "/tods-test/static/media/double_coat_blue.7b81c759.webp"
+  },
+  {
+    "revision": "85964eb3eea8f012a7592a2f95192d7c",
+    "url": "/tods-test/static/media/double_coat_blue_alt.85964eb3.webp"
+  },
+  {
+    "revision": "7608c6370dc92f7a73344b18a0bab2e1",
+    "url": "/tods-test/static/media/double_coat_brown.7608c637.webp"
+  },
+  {
+    "revision": "6d92b49229d7467cbb388cab4be8fe80",
+    "url": "/tods-test/static/media/double_coat_brown_alt.6d92b492.webp"
+  },
+  {
+    "revision": "08568d22877b53f722757f035e64f6ac",
+    "url": "/tods-test/static/media/double_coat_grey.08568d22.webp"
+  },
+  {
+    "revision": "dd24421d2527702b7066ab4ada927878",
+    "url": "/tods-test/static/media/double_coat_grey_alt.dd24421d.webp"
+  }
+]);
